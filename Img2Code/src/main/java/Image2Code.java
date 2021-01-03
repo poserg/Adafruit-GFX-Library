eaa5b07730cba13331c2f8d914dfd23387dff576
Sergey Popov <spopov@cinimex.ru>
2021-01-03 17:51:49 +0300

    Generate RGB code by image

--- Img2Code/src/main/java/Image2Code.java
+++ Img2Code/src/main/java/Image2Code.java
@@ -1,9 +1,4 @@
-import java.awt.Dimension;
-import java.awt.EventQueue;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.*;
 import java.awt.color.ColorSpace;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -105,22 +100,14 @@ public class Image2Code {
         	return out;
         }
         private String getCodeFromImg(BufferedImage in) {
-        	BufferedImage blackWhite = new BufferedImage(in.getWidth(), in.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
-    		ColorConvertOp op = new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
-            op.filter(in, blackWhite);
-
-            DataBuffer buf = blackWhite.getRaster().getDataBuffer();
-            int byteWidth = (in.getWidth() + 7) /8;
-            StringBuffer str = new StringBuffer();
-            str.append("{\n");
-            int i=0,max=buf.getSize();
-            while(i < max) {
-            	for(int j=0;j<byteWidth;++j) {
-            		str.append("0x"+Integer.toString(buf.getElem(i++),16)+(i != max ?",":""));
-            	}
-            	str.append("\n");
-    		}
-            str.append("};\n");
+			StringBuffer str = new StringBuffer();
+			for (int i = 0; i < in.getHeight(); i++) {
+				for (int j = 0; j < in.getWidth(); j++) {
+					int pixel = in.getRGB(j, i);
+					str.append(String.format("0x%06x,", pixel & 0xFFFFFF));
+				}
+				str.append("\n");
+			}
             return str.toString();
         }
         
@@ -144,7 +131,7 @@ public class Image2Code {
         void loadImage() {
             fromImg.setIcon(new ImageIcon( scale(master) ));
             blackWhite = new BufferedImage(master.getWidth(), master.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
-            ColorConvertOp op = new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
+            ColorConvertOp op = new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_LINEAR_RGB), null);
             op.filter(master, blackWhite);
             bwImg.setIcon(new ImageIcon( scale(blackWhite) ));
             codeArea.setText(getCodeFromImg(master));
